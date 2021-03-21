80bcde9205e6112a0b9e8973266fb730652c7d3f
unknown <storm00530>
2021-03-21 04:35:35 -0500

    avatar

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -5,6 +5,8 @@ import { useDispatch, useReducer } from "react-redux";
 import { logoutUser } from "../../_actions/user_action";
 import user from "../../_reducers";
 import { createStore } from "redux";
+import UserAvatar from "react-user-avatar";
+
 function Header(props) {
   const dispatch = useDispatch();
   const [menuVisible, setMenuVisible] = useState(false);
@@ -174,11 +176,7 @@ function Header(props) {
                       aria-haspopup="true"
                     >
                       <span className="sr-only">Open user menu</span>
-                      <img
-                        className="h-8 w-8 rounded-full"
-                        src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                        alt=""
-                      />
+                      <UserAvatar size="48" name="Will Binns-Smith" />
                     </button>
                   </div>
                   <UserMenu />
@@ -271,11 +269,7 @@ function Header(props) {
           <div className="pt-4 pb-3 border-t border-gray-700">
             <div className="flex items-center px-5">
               <div className="flex-shrink-0">
-                <img
-                  className="h-10 w-10 rounded-full"
-                  src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                  alt=""
-                />
+                <UserAvatar size="48" name="Will Binns-Smith" />
               </div>
               <div className="ml-3">
                 <div className="text-base font-medium leading-none text-white">
